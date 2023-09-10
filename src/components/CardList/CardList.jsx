c6963b7c61077d39b94d19c20078c1c0349952de
Svetlana Govyazina <govyazina@gmail.com>
2023-09-10 11:42:22 +0300

    new list view

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,26 +1,49 @@
-import {WordCard} from "../WordCard/WordCard";
-import {Container} from "@mui/material";
+import {Divider, List, ListItem, ListItemText} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
 import {useParams} from "react-router-dom";
 import {useEffect} from "react";
 import {getList} from "../../store/actions/mainActions";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import {ListItemSecondaryAction} from '@mui/material';
+
 
 export const CardList = () => {
-	const {words, user} = useSelector(store => store.mainStore);
-	const {id} = useParams()
-	const dispatch = useDispatch()
-	useEffect(() => {
-		if (id && user.token) {
-			dispatch(getList(user.token, id))
-		}
-	}, [id, user.token])
-	return (
-		<>
-			<Container maxWidth="sm">
-				{
-				words.map((word, index) => <WordCard key={index} front={word.front} back={word.back}/>)
-				}
-			</Container>
-		</>
-	)
+    const {words, user} = useSelector(store => store.mainStore);
+    const {id} = useParams()
+    const dispatch = useDispatch()
+    useEffect(() => {
+        if (id && user.token) {
+            dispatch(getList(user.token, id))
+        }
+    }, [id, user.token])
+    return (
+        <List sx={{width: '100%', maxWidth: 360}}>
+            {words.map((word, index) => {
+                return (
+                    <>
+                        <ListItem
+                            key={word.id}
+                        >
+                            <ListItemText
+                                primary={`${word.front}`}
+                                secondary={word.back}/>
+                            <ListItemSecondaryAction>
+                                <IconButton edge="end" aria-label="edit">
+                                    <EditIcon/>
+                                </IconButton>
+                                <IconButton edge="end" aria-label="edit">
+                                    <DeleteIcon/>
+                                </IconButton>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                        {
+                            (index < words.length - 1) && <Divider/>
+                        }
+                    </>
+                );
+            })}
+        </List>
+    );
 }
